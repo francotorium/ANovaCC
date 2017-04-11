bc2476243e6f93b609ec4a743814868418a349ca
Christopher Franco <monterrosa@eecs.berkeley.edu>
2017-04-11 14:10:12 -0700

    pokemon class ready for class

--- week5/pokemon.py
+++ week5/pokemon.py
@@ -1,8 +1,9 @@
 class Pokemon:
     _total_pokemon= 0
-    def __init__(self, _name, _type):
+    def __init__(self, _name, _type,_level, _hp):
         self._name = _name
         self._type = _type
+        self._level = _level
         Pokemon._total_pokemon += 1
 
     def getName(self):
@@ -11,6 +12,5 @@ class Pokemon:
     def getType(self):
         return self._type
 
-    @staticmethod
     def total_pokemon_caught():
         return Pokemon._total_pokemon
