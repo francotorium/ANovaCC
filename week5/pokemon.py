447983889ba33d961916812fef6c484d3a305e3d
Christopher Franco <monterrosa@eecs.berkeley.edu>
2017-04-11 15:35:26 -0700

    added a pokemon subclass

--- week5/pokemon.py
+++ week5/pokemon.py
@@ -4,6 +4,7 @@ class Pokemon:
         self._name = _name
         self._type = _type
         self._level = _level
+        self._hp= _hp
         Pokemon._total_pokemon += 1
 
     def getName(self):
@@ -12,5 +13,18 @@ class Pokemon:
     def getType(self):
         return self._type
 
+    def getLevel(self):
+        return self._level
+
+    def getHp(self):
+        return self._hp
+
     def total_pokemon_caught():
         return Pokemon._total_pokemon
+
+
+class Charmander(Pokemon):
+    def __init__(self, _name, _type,_level, _hp):
+        Pokemon.__init__(self, _name, _type,_level, _hp)
+    def emberAttack(self):
+        print("charmander spits dat fire")
